8293273795b5cdb7374419e79a769ccd68acfcb1
scottw84 <scottwwj16@gmail.com>
2023-04-15 20:09:07 +0800

    LoginCLI UI done.

--- src/Boundary/LoginCLI.java
+++ src/Boundary/LoginCLI.java
@@ -1,7 +1,9 @@
 package src.Boundary;
 
+import java.io.Console;
 import java.util.Scanner;
 
+import src.ConsoleColors;
 import src.Controller.UserController;
 import src.Entity.User;
 import java.util.List;
@@ -23,10 +25,10 @@ public class LoginCLI {
         String password = scanner.nextLine();
         User user = userController.loginUser(users, userPasswords, userID, password);
         if (user != null) {
-            System.out.println("Login successful!");
+            System.out.println(ConsoleColors.GREEN_BRIGHT + "Login successful!" + ConsoleColors.RESET);
             return user;
         } else {
-            System.out.println("Invalid user ID or password.");
+            System.out.println(ConsoleColors.RED_BRIGHT + "Invalid user ID or password. Please try again!\n" + ConsoleColors.RESET);
             return null;
         }
     }
